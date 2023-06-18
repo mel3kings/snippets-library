ce49bf729bc4aa958b72cff6d26b56f0230e29d9
Melchor Tatlonghari <melchor.tatlonghari@service.nsw.gov.au>
2023-06-18 18:25:00 +1000

    feat parameterize

--- scripts/python/youtube-dl/download.py
+++ scripts/python/youtube-dl/download.py
@@ -1,6 +1,12 @@
+import argparse
 import pytube
 
-url = input("Enter the URL of the video: ")
-yt = pytube.YouTube(url)
+# Create argument parser
+parser = argparse.ArgumentParser(description='Download a YouTube video.')
+parser.add_argument('url', help='URL of the video')
+args = parser.parse_args()
+
+# Download the video
+yt = pytube.YouTube(args.url)
 stream = yt.streams.get_highest_resolution()
 stream.download()
